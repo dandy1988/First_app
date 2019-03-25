d578fc8b037b7cb94660f555817e83504445336a
dandy1988 <dandy1988@ukr.net>
2019-03-25 17:25:49 +0200

    DZ2 main part

--- app/src/main/java/net/ukr/dandy1988/first_app/MainActivity.java
+++ app/src/main/java/net/ukr/dandy1988/first_app/MainActivity.java
@@ -1,11 +1,18 @@
 package net.ukr.dandy1988.first_app;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,6 +21,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         EditText etName = findViewById(R.id.etName);
+
+     //   Spannable text = new SpannableString(".");
+     //   text.setSpan(new UnderlineSpan(), 8, 17, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+     //   text.setSpan(new StyleSpan(Typeface.BOLD), 22, 26, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        TextView etPriority = findViewById(R.id.etPriority);
+        etPriority.setText("Hello");
+
         final Button btnAdd = findViewById(R.id.btnAdd);
         btnAdd.setEnabled(false);
         etName.addTextChangedListener(new TextWatcher() {
