ac6b6a4eae2cdbff8787904c457824dc8130f870
dandy1988 <dandy1988@ukr.net>
2019-04-07 22:09:15 +0300

    DZ4 part2

---
+++ app/src/main/java/net/ukr/dandy1988/first_app/TaskClickListener.java
@@ -0,0 +1,6 @@
+package net.ukr.dandy1988.first_app;
+
+//клик на задачу
+public interface TaskClickListener {
+    public void onClick(Task task);
+}
---
+++ app/src/main/java/net/ukr/dandy1988/first_app/TaskViewHolder.java
@@ -0,0 +1,32 @@
+package net.ukr.dandy1988.first_app;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.view.View;
+import android.widget.TextView;
+
+//тут ViewHolder
+public class TaskViewHolder extends RecyclerView.ViewHolder{
+    private TextView tvTaskName;
+
+    public TaskViewHolder(@NonNull View itemView){
+        super(itemView);
+        tvTaskName = itemView.findViewById(R.id.tvTaskName);
+    }
+
+    public void setData(Task task) {
+        Spannable text = new SpannableString( "•   "+task.getName());
+        if (task.getColor()>4){
+            text.setSpan(new ForegroundColorSpan(Color.RED), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+
+        }else{
+            text.setSpan(new ForegroundColorSpan(Color.GREEN), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        }
+        tvTaskName.setText(text);
+    }
+}
