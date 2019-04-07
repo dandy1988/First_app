9e66913dbaa24ffef8dbfa9b9315e4ff2a743788
dandy1988 <dandy1988@ukr.net>
2019-04-07 21:48:30 +0300

    DZ4 part2

--- app/src/main/java/net/ukr/dandy1988/first_app/TasksActivity.java
+++ app/src/main/java/net/ukr/dandy1988/first_app/TasksActivity.java
@@ -1,26 +1,18 @@
 package net.ukr.dandy1988.first_app;
 
-import android.content.Intent;
-import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.text.Spannable;
-import android.text.SpannableString;
-import android.text.Spanned;
-import android.text.style.ForegroundColorSpan;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class TasksActivity extends AppCompatActivity {
 
@@ -30,52 +22,37 @@ public class TasksActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
+        ViewPager vp = findViewById(R.id.vp);
+        TabLayout tl = findViewById(R.id.tl);
 
+        PagerAdapter adapter = new PageAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+        tl.setupWithViewPager(vp);
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == ADD_TASK_REQUEST_CODE && resultCode == RESULT_OK){
-            if (data != null){
-                Task task = ((Task) data.getSerializableExtra(Task.class.getName()));
-                Toast.makeText(this, task.getName(), Toast.LENGTH_SHORT).show();
-            }
+    public class PageAdapter extends FragmentStatePagerAdapter{
+        public PageAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Nullable
+        @Override
+        public CharSequence getPageTitle(int position) {
+            return "Tasks";
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return new TasksFragment();
+        }
+
+        @Override
+        public int getCount() {
+            return 2;
         }
     }
 
-    public List<Task> generatedFakeData(){
-        List<Task> tasks = new ArrayList<>();
-        for (int i = 0; i < 50; i++) {
-            int colorOfTaskPiority = (int) (10*Math.random());
-            tasks.add( new Task("Task #"+i+"  ",3,colorOfTaskPiority));
-        }
-        return  tasks;
-    }
 
-    //тут ViewHolder
-    public static class TaskViewHolder extends RecyclerView.ViewHolder{
-        private TextView tvTaskName;
 
-        public TaskViewHolder(@NonNull View itemView){
-            super(itemView);
-            tvTaskName = itemView.findViewById(R.id.tvTaskName);
-        }
 
-        public void setData(Task task) {
-            Spannable text = new SpannableString( "•   "+task.getName());
-            if (task.getColor()>4){
-                text.setSpan(new ForegroundColorSpan(Color.RED), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-            }else{
-                text.setSpan(new ForegroundColorSpan(Color.GREEN), 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-            }
-            tvTaskName.setText(text);
-        }
-    }
-
-    //клик на задачу
-    public interface TaskClickListener{
-        public void onClick(Task task);
-    }
 }
